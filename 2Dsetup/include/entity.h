0cd3962d92337d9c14871bf96dfe3de622963fdb
JoeTobon <jet33@njit.edu>
2017-09-14 10:19:52 -0400

    started to implement entity system

---
+++ 2Dsetup/include/entity.h
@@ -0,0 +1,26 @@
+#include <SDL.h>
+#include "gf2d_sprite.h"
+#include "gf2d_types.h"
+#include "gf2d_vector.h"
+#include "gf2d_text.h"
+
+typedef struct Entity_S
+{
+	Vector2D position;						 //where the entity appears on the screen (using x and y coordinates)
+	Vector2D velocity;						 //how fast the enity is moving in a given direction
+	float scale;
+	int health;								 //keeps track of entity's health
+	int frame;								 //determines which frame the entity is on
+	Sprite sprite;							 //sprite associated with entity 
+	Bool INUSE;								 //used to check if entity is in use
+
+	void (*update)(struct Entity_S *self);  //used to update the enity's current states
+
+
+}Entity;
+
+/**
+ * @brief  initializes the enity manager
+ * @param  maximum number of enities the system can handle
+ */
+void enity_system_init(Uint32 maxNum);
